14b8212fde7acb772876f87daa43a55742d233c0
Masoud2020 <akam_50@yahoo.com>
2019-12-17 10:08:11 +0100

    update

--- MyProject/src/HelloWorld.java
+++ MyProject/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		//TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 		String name = "Masoud";
 		System.out.println("Bonjour " + name +" !"); 
 		
@@ -20,6 +21,8 @@ public class HelloWorld {
 		boolean p = (a > b) || (b < 65);
 		
 		System.out.println(p);
+		
+		
 		
 		
 		
