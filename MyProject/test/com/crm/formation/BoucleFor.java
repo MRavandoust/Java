2562cd74b89eb7c21a0ac08754079e048a7036d9
Masoud2020 <akam_50@yahoo.com>
2020-01-15 16:56:00 +0100

    15/01/2020 Boucles, jou, dessiner

---
+++ MyProject/test/com/crm/formation/BoucleFor.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class BoucleFor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		float i;
+		for(i = 0; i < 20 ; i++) {
+			System.out.format("i vaut : %.2f \n", i);
+		}
+
+	}
+
+}
---
+++ MyProject/test/com/crm/formation/DessinerUnCarré.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+import java.util.Scanner;
+
+/**
+ * @author CRM
+ *
+ */
+public class DessinerUnCarré {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez une longueur du triangle :");
+		int k = sc.nextInt();
+		int i;
+		int j;
+		for (i = 0; i < k; i++) {
+			for (j = 0; j < k; j++)
+				System.out.print("*");
+			System.out.print("\n");
+		}
+
+	}
+
+}
