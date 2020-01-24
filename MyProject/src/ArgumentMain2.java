52f4e07fd3e2fedd609449e0c5e50874ed8da514
Masoud2020 <akam_50@yahoo.com>
2020-01-24 09:58:28 +0100

    24/01/2020

---
+++ MyProject/src/ArgumentMain2.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+* @author Masoud RAVANDOUST
+*/
+
+/**
+ * @author CRM
+ *
+ */
+public class ArgumentMain2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args.length == 4) {
+			System.out.println("Bienvenue " + args[0] + " " + args[1] + " " + args[2]);
+			System.out.println("Il est " + args[3]);
+		}
+
+	}
+
+}
--- MyProject/test/com/crm/formation/LesChiffreEntreDeuxLimit.java
+++ MyProject/test/com/crm/formation/LesChiffreEntreDeuxLimit.java
@@ -6,16 +6,14 @@ package com.crm.formation;
 import java.util.Scanner;
 
 /**
-* Masoud RAVANDOUST
-*/
-
-/**
+ * 
  * @author CRM
  *
  */
 public class LesChiffreEntreDeuxLimit {
 
 	/**
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -25,7 +23,7 @@ public class LesChiffreEntreDeuxLimit {
 		System.out.println("Entre deuxième nombre: ");
 		int n2 = sc.nextInt();
 		System.out.println("Le valeurs intermédiaires :");
-		 
+
 		//premier type repetative
 //		for(int i=0; i < ((n2-n1)/2)+1; i++)
 //			System.out.println(n1 + 2*i);
@@ -43,7 +41,8 @@ public class LesChiffreEntreDeuxLimit {
 			System.out.println(n1 + 2*i);
 			i++;
 		} while (i < ((n2-n1)/2)+1) ;
-			
+		
+		sc.close();
 	}
 
 }
